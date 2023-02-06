95db32b961d99ec8c918826a720e6a3ea6267f16
Hanna Lytvynenko <97547547+Ani-k-a@users.noreply.github.com>
2023-02-06 20:01:45 +0100

    task 12 update

--- project_12/hw/script.js
+++ project_12/hw/script.js
@@ -39,20 +39,113 @@ const products = [
     }
 ]
 
+
+const productForm = document.querySelector('.section-add');
+const productName = document.querySelector('.add-product_name');
+const productPrice = document.querySelector('.add-product_price');
+
 const productsList = document.querySelector('.product-list');
 
-products.forEach(el => {
-    productsList.innerHTML += `
-    <li class="section section-product">
+const paramsSort = document.querySelector('.sort-select');
+const namesSort = document.querySelector('.sort-item');
+
+
+function renderProducts() {
+    productsList.innerHTML = '';
+    products.forEach(el => {
+        productsList.innerHTML += `
+    <li class="section section-product" id="${el.id}">
         <div class="product-name">${el.name}</div>
         <div class="product-price">${el.price}</div>
+        <button class="delete-btn"><svg class="delete-icon"><use href="./symbol-defs.svg#icon-modal_close"></use></svg></button>
     </li>
     `
-});
+    }); 
+   
+    onHover();
+    deleteProduct();
+};
+renderProducts();
 
-const productNameAdd = document.querySelector('.add-product_name');
-const productPriceAdd = document.querySelector('.add-product_price');
-const productAddButton = document.querySelector('.add-btn');
+function addProducts (ev) {
+    ev.preventDefault();
+    products.push({
+        id: products.length + 1,
+        name: productName.value,
+        price: Number(productPrice.value),
+    })
+    renderProducts();
+    clearInput();
+};
 
+function clearInput() {
+    productName.value = '';
+    productPrice.value = '';
+}
+productForm.addEventListener('submit', addProducts);
+
+
+function onHover() {
+    const productCard = document.querySelectorAll('.section-product');
+    productCard.forEach(function (el) {
+        el.addEventListener('mouseover', function () {
+            const deleteBtn = el.querySelector('.delete-btn');
+            deleteBtn.style.display = 'flex';
+        })
+
+        el.addEventListener('mouseout', function () {
+            const deleteBtn = el.querySelector('.delete-btn');
+            deleteBtn.style.display = 'none';
+        })
+    })
+};
+onHover();
+
+function deleteProduct() {
+    const deleteBtns = document.querySelectorAll('.delete-btn');
+    deleteBtns.forEach(function (el, ind) {
+        el.addEventListener('click', function() {
+            el.parentElement.remove();
+            products.splice(ind, 1); 
+            renderProducts();   
+        })   
+    })
+};
+
+function sortByParams () {
+    paramsSort.addEventListener('change', function(){
+            if (paramsSort.value === "Price") {
+                products.sort((firstEl, secondEl) => firstEl.price - secondEl.price);
+                renderProducts()                
+            } else if (paramsSort.value === "Name") {
+                products.sort((firstEl, secondEl)=> firstEl.name.localeCompare(secondEl.name));
+                renderProducts()
+            } else renderProducts();
+        })
+}
+sortByParams();
+
+
+
+// IN PROCESS
+
+// function sortByName () {
+//     namesSort.addEventListener('input', function(ev){
+       
+//         products.filter(el => el.name === ev.currentTarget.value)
+//         .forEach(el => {
+//             productsList.innerHTML += `
+//         <li class="section section-product" id="${el.id}">
+//             <div class="product-name">${el.name}</div>
+//             <div class="product-price">${el.price}</div>
+//             <button class="delete-btn"><svg class="delete-icon"><use href="./symbol-defs.svg#icon-modal_close"></use></svg></button>
+//         </li>
+//         `
+//         })
+//         });
+        
+
+// }
+// sortByName();
 
 
